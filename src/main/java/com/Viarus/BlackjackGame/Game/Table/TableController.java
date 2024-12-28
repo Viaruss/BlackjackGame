fcb5667d4fd59b3e09762798cee190188029f0ec
Viaruss <bjartoszewicz@gmail.com>
2024-12-28 19:47:40 +0100

    Cleanup: * removed unused endpoints from table controller * removed old frontend files (pre vue migration)

--- src/main/java/com/Viarus/BlackjackGame/Game/Table/TableController.java
+++ src/main/java/com/Viarus/BlackjackGame/Game/Table/TableController.java
@@ -74,27 +74,4 @@ public class TableController {
                     .body(e.getMessage());
         }
     }
-
-/*    TODO: Might be useless, should be automatically called by a scheduler
-    @PostMapping("/startGame/{tableId}")
-    public ResponseEntity<Object> startGame(@PathVariable String tableId) {
-        try {
-            Table table = tableService.startGame(tableId);
-            simpMessagingTemplate.convertAndSend("/topic/table/" + table.getId(), table);
-            return ResponseEntity.ok(table);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest()
-                    .body(e.getMessage());
-        }
-    }*/
-/*  TODO: Might be useless, should be automatically called by a scheduler
-    @GetMapping("roundResult/{tableId}")
-    public ResponseEntity<Table> roundResult(@PathVariable String tableId) {
-        Table table = tableService.roundResult(tableId);
-
-        if (table == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        return ResponseEntity.ok(table);
-    }*/
 }
